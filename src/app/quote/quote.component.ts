48efb9c5aab1f944c0aa6de966539381cb18ae50
Naomi Ndibui <naomi.ndibui@gmail.com>
2019-03-26 08:14:57 +0300

    initial commit

---
+++ src/app/quote/quote.component.ts
@@ -0,0 +1,32 @@
+import { Component, OnInit } from '@angular/core';
+import { Quote } from '../quote'
+
+@Component({
+  selector: 'app-quote',
+  templateUrl: './quote.component.html',
+  styleUrls: ['./quote.component.css']
+})
+export class QuoteComponent implements OnInit {
+
+  quotes = [
+    new Quote(1, 'Spread love everywhere you go. Let no one ever come to you without leaving happier.','Mother Teresa',"Naomi"),
+    new Quote(2, 'When you reach the end of your rope, tie a knot in it and hang on.','Franklin D Roosevelt',"Naomi"),
+    new Quote(3, 'Whoever is happy will make others happy too.','Anne Frank',"Naomi"),
+];
+
+addNewQuote(quote){
+  let quoteLength = this.quotes.length;
+  quote.id=quoteLength+1;
+  this.quotes.unshift(quote)
+
+}
+
+toogleDetails(index){
+  this.quotes[index].showAuthor = !this.quotes[index].showAuthor;
+}
+  constructor() { }
+
+  ngOnInit() {
+  }
+
+}
