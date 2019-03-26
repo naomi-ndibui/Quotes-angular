d95817729b7cd0de2e0d3ed9c3a4090803ad7647
Naomi Ndibui <naomi.ndibui@gmail.com>
2019-03-26 13:01:29 +0300

    added and deleted

--- src/app/count.pipe.spec.ts
+++
@@ -1,7 +0,0 @@
-import { Count.Pipe } from './count.pipe';
-
-describe('Count.Pipe', () => {
-  it('should create an instance', () => {
-    expect(new Count.Pipe()).toBeTruthy();
-  });
-});
--- src/app/quote-form/quote-form.component.ts
+++ src/app/quote-form/quote-form.component.ts
@@ -8,7 +8,7 @@ import { Quote } from '../quote'
 })
 export class QuoteFormComponent implements OnInit {
 
-  newQuote=new Quote(0,"","","");
+  newQuote=new Quote(0,"","","",new Date());
     @Output() addQuote=new EventEmitter<Quote>();
 
 
