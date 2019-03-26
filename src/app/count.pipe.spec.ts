94151ae3b4c798eef01450454d71877bdeea7949
Naomi Ndibui <naomi.ndibui@gmail.com>
2019-03-26 11:08:23 +0300

    initial commit

---
+++ src/app/count.pipe.spec.ts
@@ -0,0 +1,7 @@
+import { Count.Pipe } from './count.pipe';
+
+describe('Count.Pipe', () => {
+  it('should create an instance', () => {
+    expect(new Count.Pipe()).toBeTruthy();
+  });
+});
---
+++ src/app/count.pipe.ts
@@ -0,0 +1,2 @@
+export class Count.Pipe {
+}
