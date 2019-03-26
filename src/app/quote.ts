d96daccb01ebde4bceba099cdc2fb1ee7425ef71
Naomi Ndibui <naomi.ndibui@gmail.com>
2019-03-26 11:22:41 +0300

    added a date constructor to give it a date

--- src/app/quote.ts
+++ src/app/quote.ts
@@ -1,6 +1,6 @@
 export class Quote {
     public showAuthor:boolean;
-    constructor(public id:number, public name:string,public author:string,public submitted:string){
+    constructor(public id:number, public name:string,public author:string,public submitted:string,public completeDate:Date){
         this.showAuthor=false
 
     }
