a66d52021d54c11c571e78076a2dc271a185df9d
Naomi Ndibui <naomi.ndibui@gmail.com>
2019-03-26 11:23:15 +0300

    added a date

--- src/app/quote/quote.component.ts
+++ src/app/quote/quote.component.ts
@@ -9,9 +9,9 @@ import { Quote } from '../quote'
 export class QuoteComponent implements OnInit {
 
   quotes = [
-    new Quote(1, 'Spread love everywhere you go. Let no one ever come to you without leaving happier.','Mother Teresa',"Naomi"),
-    new Quote(2, 'When you reach the end of your rope, tie a knot in it and hang on.','Franklin D Roosevelt',"Naomi"),
-    new Quote(3, 'Whoever is happy will make others happy too.','Anne Frank',"Naomi"),
+    new Quote(1, 'Spread love everywhere you go. Let no one ever come to you without leaving happier.','Mother Teresa',"Naomi",new Date(2019,3,26)),
+    new Quote(2, 'When you reach the end of your rope, tie a knot in it and hang on.','Franklin D Roosevelt',"Naomi",new Date(2019,3,26)),
+    new Quote(3, 'Whoever is happy will make others happy too.','Anne Frank',"Naomi",new Date(2019,3,26)),
 ];
 
 
